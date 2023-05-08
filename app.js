08d6e20c4479594fb48bca6b01426fc28aeb14b8
Md-shefat-masum <mshefat924@gmail.com>
2023-05-08 09:06:41 +0600

    work start time append

--- app.js
+++ app.js
@@ -1,15 +1,102 @@
 let dashboard = Vue.component("dashboard", {
-    created: function () {
-        console.log("dashboard init");
-    },
-    template: '<div>dashboard</div>'
-})
+	created: function () {
+		console.log("dashboard init");
+	},
+	methods: {},
+	template: `
+        <section class="my-5">
+            <h2 class="mb-5">Today Progress</h2>
+            <div class="row gy-3">
+                <div class="col-md-4">
+
+                    <div class="card progress_cards shadow-sm border-0">
+                        <div class="card-body">
+                            <h3 class="heading">Activity</h3>
+                            <div class=" bottom d-flex justify-content-between">
+                                <div class="progresss d-flex">
+                                    <div class="qty">00</div>%
+                                </div>
+                                <div class="icon">
+                                    <i class="fa-solid fa-arrow-trend-up"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card  progress_cards shadow-sm border-0">
+                        <div class="card-body">
+                            <h3 class="heading">Completed task</h3>
+                            <div class=" bottom d-flex justify-content-between">
+                                <div class="progresss d-flex">
+                                    <div class="qty">00</div>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa-solid fa-list-check"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="mb-5 mt-5">Last week Progress</h2>
+            <div class="row gy-3">
+                <div class="col-md-4">
+                    <div class="card  progress_cards shadow-sm border-0">
+                        <div class="card-body">
+                            <h3 class="heading">Weekly Activity</h3>
+                            <div class=" bottom d-flex justify-content-between">
+                                <div class="progresss d-flex">
+                                    <div class="qty">20</div>%
+                                </div>
+                                <div class="icon">
+                                    <i class="fa-solid fa-arrow-trend-up"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card  progress_cards shadow-sm border-0">
+                        <div class="card-body">
+                            <h3 class="heading">Worked this week</h3>
+                            <div class=" bottom d-flex justify-content-between">
+                                <div class="progresss d-flex">
+                                    <div class="qty">20:23:00</div> min
+                                </div>
+                                <div class="icon">
+                                    <i class="far fa-clock"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card  progress_cards shadow-sm border-0">
+                        <div class="card-body">
+                            <h3 class="heading">Completed task</h3>
+                            <div class=" bottom d-flex justify-content-between">
+                                <div class="progresss d-flex">
+                                    <div class="qty">20</div>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa-solid fa-list-check"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    `,
+});
 let tasks = Vue.component("task", {
-    created: function () {
-        console.log("task init");
-    },
-    template: '<div>task</div>'
-})
+	created: function () {
+		console.log("task init");
+	},
+	template: "<div>task</div>",
+});
 
 const routes = [
 	{ path: "/", component: dashboard },
@@ -18,6 +105,7 @@ const routes = [
 
 const router = new VueRouter({
 	routes, // short for `routes: routes`
+	linkExactActiveClass: "active",
 });
 
 new Vue({
@@ -25,50 +113,56 @@ new Vue({
 	el: "#app",
 	data: function () {
 		return {
-			tasks: [],
-			selected_el: {},
+			start_time: null,
+			tracker_interval: null,
+			working_hour: null,
+			tracker_control: null,
 		};
 	},
 	created: function () {
-		console.log("hello");
+        this.$nextTick(function () {
+        });
+	},
+	updated: function () {
+        this.$nextTick(function () {
+        });
+	},
+	methods: {
+		start_time_tracker: function () {
+			this.working_hour = document.getElementById("working_hour");
+			this.tracker_control = document.querySelector(".tracker_control");
+			if (this.tracker_control.classList.contains("fa-play")) {
+				this.tracker_control.classList.remove("fa-play");
+				this.tracker_control.classList.add("fa-pause");
+				start_time = moment();
+
+				tracker_interval = setInterval(() => {
+					var diff = moment.duration(moment().diff(start_time));
+					this.working_hour.innerHTML = "";
+					this.working_hour.innerHTML += diff.hours().toString().padStart(2, "0") + ":";
+					this.working_hour.innerHTML += diff.minutes().toString().padStart(2, "0") + ":";
+					this.working_hour.innerHTML += diff.seconds().toString().padStart(2, "0");
+				}, 1000);
+
+				// google.script.run.set_track_work_time({
+				// 	date: new Date().toDateString(),
+				// 	start_time: moment().format("HH:MM:SS"),
+				// 	end_time: "",
+				// });
+			} else {
+				this.tracker_control.classList.remove("fa-pause");
+				this.tracker_control.classList.add("fa-play");
+				clearInterval(tracker_interval);
+				// google.script.run.set_track_work_time({
+				// 	date: new Date().toDateString(),
+				// 	start_time: "",
+				// 	end_time: moment().format("HH:MM:SS"),
+				// });
+			}
+		},
 	},
 });
 
 setInterval(() => {
 	document.getElementById("today_date").innerHTML = new moment().format("ddd DD MMM, YYYY | hh:mm:ss a");
 }, 1000);
-
-let start_time = null;
-let tracker_interval = null;
-let working_hour = document.getElementById("working_hour");
-let tracker_control = document.querySelector(".tracker_control");
-function start_time_tracker() {
-	if (tracker_control.classList.contains("fa-play")) {
-		tracker_control.classList.remove("fa-play");
-		tracker_control.classList.add("fa-pause");
-		start_time = moment();
-
-		tracker_interval = setInterval(() => {
-			var diff = moment.duration(moment().diff(start_time));
-			working_hour.innerHTML = "";
-			working_hour.innerHTML += diff.hours().toString().padStart(2, "0") + ":";
-			working_hour.innerHTML += diff.minutes().toString().padStart(2, "0") + ":";
-			working_hour.innerHTML += diff.seconds().toString().padStart(2, "0");
-		}, 1000);
-
-		// google.script.run.set_track_work_time({
-		// 	date: new Date().toDateString(),
-		// 	start_time: moment().format("HH:MM:SS"),
-		// 	end_time: "",
-		// });
-	} else {
-		tracker_control.classList.remove("fa-pause");
-		tracker_control.classList.add("fa-play");
-		clearInterval(tracker_interval);
-		// google.script.run.set_track_work_time({
-		// 	date: new Date().toDateString(),
-		// 	start_time: "",
-		// 	end_time: moment().format("HH:MM:SS"),
-		// });
-	}
-}
